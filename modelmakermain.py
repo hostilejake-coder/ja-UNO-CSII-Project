cd1790629f3f51548b7b07b6983aa52d8927bd9f
hostilejake-coder <hostilejake@gmail.com>
2026-05-03 03:24:42 -0500

    Added exception handling (for imports)

--- modelmakermain.py
+++ modelmakermain.py
@@ -1,7 +1,13 @@
-from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QApplication
-import sys
-from gui_logic import MainWindow
+"""
+CSCI-1620 — Startup Module
+"""
+try:
+    from PyQt6 import QtWidgets
+    from PyQt6.QtWidgets import QApplication
+    import sys
+    from gui_logic import MainWindow
+except Exception as e:
+    print(f"Error loading imports: {e}")
 
 if __name__ == "__main__":
     print("Creating window... Please wait.")
